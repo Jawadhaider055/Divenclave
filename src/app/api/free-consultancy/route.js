ec7b24c1bc98cc882e8b0a823bad1f429054b5f2
Jawad Haider <imjawadhaider@gmail.com>
2024-10-29 13:46:58 +0500

    first commit

--- src/app/api/free-consultancy/route.js
+++ src/app/api/free-consultancy/route.js
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '../../lib/prisma';
 
 export async function POST(req, res) {
   const { firstName, email, phone, website, consultancy, message } = await req.json();
